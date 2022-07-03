c8bd03dcc9c3be9681b1bc5cc4e31fe75fa2ed4c
sgaona21 <stevegaona21@gmail.com>
2022-07-03 14:13:08 -0700

    steps 1-4

--- js/app.js
+++ js/app.js
@@ -9,3 +9,11 @@ const overlay = document.getElementById('overlay');
 startGameButton.addEventListener('click', () => {
     overlay.style.display = 'none';
 })
+
+const phrases = [
+    'dont squat with your spurs',
+    'gym tan laundry',
+    'peanut butter and jelly',
+    'chicken fried rice',
+    'stay hydrated'
+]
